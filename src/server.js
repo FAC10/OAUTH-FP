e62ceff096fa6517f1da6dde7f4246667e17dcf4
pbywater <philippabywater@gmail.com>
2017-04-10 14:27:36 +0100

    adding necessary dependencies and getting https working
    relates to #10

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const hapi = require('hapi');
 const inert = require('inert');
+const path = require('path');
 const server = new hapi.Server();
+const fs = require('fs');
 
 const routes = require('./routes');
 
-server.connection({ port: 4000, host: 'localhost' });
+server.connection({ port: process.env.PORT || 3000,
+  tls: {
+        key: fs.readFileSync(path.join(__dirname, '../keys/key.pem'), 'utf8'),
+        cert: fs.readFileSync(path.join(__dirname, '../keys/cert.pem'), 'utf8')
+    }
+});
+
+
 
 server.register(inert, (err) => {
   if (err) throw err;
