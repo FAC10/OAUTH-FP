0aaf9c99ba3a2d8b9e4761a8c656a5bc72a40076
pbywater <philippabywater@gmail.com>
2017-04-11 12:28:12 +0100

    added hapi auth cookie

--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -1,20 +1,22 @@
 const request = require('request');
 const env2 = require('env2')('./config.env');
-const bcrypt = require('bcrypt');
 
 module.exports = {
   method: 'GET',
   path: '/welcome',
+  config: {
+    auth: false,
+  },
   handler: (req, reply) => {
     const query = req.url.query;
     const clientId = process.env.CLIENT_ID;
     const clientSecret = process.env.CLIENT_SECRET;
     request.post(`https://github.com/login/oauth/access_token?code=${query.code}&client_id=${clientId}&client_secret=${clientSecret}`, function (err, res, accessToken){
       if (err) console.log(err);
-      bcrypt.hash(accessToken, 10, function(err, hash) {
-        const session = {accessToken: hash, last: Date.now()};
-        reply.redirect('/').state('oAuthWorkshop', session);
-      });
+      else {
+        req.cookieAuth.set({ accessToken });
+    }
+        reply.redirect('/');
     });
   },
 };
